5ff8504a703d53f3df79f307c2a76aa947792b15
trickster131 <97235972+trickster131@users.noreply.github.com>
2022-01-14 19:41:07 +0530

    Add files via upload

---
+++ output.py
@@ -0,0 +1,32 @@
+Python 3.10.1 (tags/v3.10.1:2cd268a, Dec  6 2021, 19:10:37) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+========= RESTART: C:\Users\Vivek Tiwari\Downloads\PYTHON ASSIGNMENT.py ========
+ENTER THE FIRST NUMBER:22
+ENTER THE SECOND NUMBER:25
+ENTER THE THIRD NUMBER:23
+THE AVERAGE OF THREE NUMBERS Is  23 
+
+ENTER YOUR GROSS INCOME:20000
+ENTER THE NUMBER OF DEPENDANTS:2
+TAX TO BE PAID IS: 800 
+
+ENTER YOUR SID:21104037
+ENTER YOUR NAME:Vivek Tiwari
+ENTER YOUR GENDER:Male
+ENTER YOUR COURSE NAME:Introduction to Computing
+ENTER YOUR CGPA:10
+['21104037', 'Vivek Tiwari', 'Male', 'Introduction to Computing', '10'] 
+
+MARKS OF FIRST STUDENT:77
+MARKS OF SECOND STUDENT:79
+MARKS OF THIRD STUDENT:80
+MARKS OF FOURTH STUDENT:72
+MARKS OF FIFTH STUDENT:71
+['71', '72', '77', '77', '79', '80'] 
+ 
+
+['Red', 'Green', 'White', 'Pink', 'Yellow'] 
+
+['Red', 'Green', 'White', 'Purple', 'Pink', 'Yellow']
+['Red', 'Green', 'White', 'Black', 'Purple', 'Yellow']
