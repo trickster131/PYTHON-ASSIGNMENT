b17335459137d25a8bd706ec972c3fc517121017
trickster131 <97235972+trickster131@users.noreply.github.com>
2022-01-14 19:33:55 +0530

    Delete OUTPUT FILE.py

--- OUTPUT FILE.py
+++
@@ -1,5 +0,0 @@
-Python 3.10.1 (tags/v3.10.1:2cd268a, Dec  6 2021, 19:10:37) [MSC v.1929 64 bit (AMD64)] on win32
-Type "help", "copyright", "credits" or "license()" for more information.
-
-========= RESTART: C:\Users\Vivek Tiwari\Downloads\PYTHON ASSIGNMENT.py ========
-ENTER THE FIRST NUMBER:
