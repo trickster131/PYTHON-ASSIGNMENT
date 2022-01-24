32ac53b6c8e51337007932a30e24d2aa0fb0575d
trickster131 <97235972+trickster131@users.noreply.github.com>
2022-01-24 19:32:20 +0530

    Add files via upload

---
+++ OUTPUT FILE 2.py
@@ -0,0 +1,64 @@
+Python 3.10.1 (tags/v3.10.1:2cd268a, Dec  6 2021, 19:10:37) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+======== RESTART: C:/Users/Vivek Tiwari/Downloads/PYTHON ASSIGNMENT 2.py =======
+PYTHON ASSIGNMENT 2
+
+<<<<<<<<<<<QUESTION 1>>>>>>>>>>>
+35 
+
+egaugnal evitisnes esac a si nohtyP 
+
+a case sensitive 
+
+Python is object oriented language 
+
+10 
+
+Pythonisacasesensitivelanguage 
+
+
+<<<<<<<<<<<QUESTION 2>>>>>>>>>>>
+Program to store name, SID, department name and CGPA into different variables
+
+Hey, VIVEK TIWARI Here! 
+
+My SID is 21104037 
+I am from ELECTRICAL ENGINEERING department and my CGPA is 10 
+
+
+<<<<<<<<<<<QUESTION 3>>>>>>>>>>>
+a&b= 8
+a|b= 58
+a^b= 50
+a << 2 = 224
+b << 2 = 40
+a >> 2 = 14
+b >> 4 = 0 
+
+
+<<<<<<<<<<<QUESTION 4>>>>>>>>>>>
+A Python program to find the greatest of three numbers entered by the user
+
+ENTER FIRST NUMBER: 23
+ENTER SECOND NUMBER: 21
+ENTER THIRD NUMBER: 25
+25 is greatest
+
+
+
+<<<<<<<<<<<QUESTION 5>>>>>>>>>>>
+Program to check if the word “name” is present in the string entered by the user
+
+ENTER YOUR STRING: My name is Vivek
+YES
+
+
+
+<<<<<<<<<<<QUESTION 6>>>>>>>>>>>
+Program to check whether a triangle can be formed or not
+
+ENTER FIRST SIDE LENGTH: 21
+ENTER SECOND SIDE LENGTH: 13
+ENTER THIRD SIDE LENGTH: 37
+NO TRIANGLE CANNOT BE FORMED
