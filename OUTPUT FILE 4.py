f2c010aa73f7091724de00fbf438af562bbf523d
trickster131 <97235972+trickster131@users.noreply.github.com>
2022-02-27 20:03:55 +0530

    Add files via upload

---
+++ OUTPUT FILE 4.py
@@ -0,0 +1,81 @@
+Python 3.10.1 (tags/v3.10.1:2cd268a, Dec  6 2021, 19:10:37) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+======== RESTART: C:/Users/Vivek Tiwari/Downloads/PYTHON ASSIGNMENT 4.py =======
+<<<<<QUESTION 1>>>>>
+Move disk 1 from rod A to rod C
+Move disk 2 from rod A to rod B
+Move disk 1 from rod C to rod B
+Move disk 3 from rod A to rod C
+Move disk 1 from rod B to rod A
+Move disk 2 from rod B to rod C
+Move disk 1 from rod A to rod C
+
+
+
+<<<<<QUESTION 2>>>>>
+Enter the size of pascals triangle 6
+USING FOR LOOP
+       1 
+      1 1 
+     1 2 1 
+    1 3 3 1 
+   1 4 6 4 1 
+  1 5 10 10 5 1 
+
+
+
+USING WHILE LOOP
+       1 
+      1 1 
+     1 2 1 
+    1 3 3 1 
+   1 4 6 4 1 
+  1 5 10 10 5 1 
+
+
+
+USING RECURSSIONS
+          1   
+        1   1   
+      1   2   1   
+    1   3   3   1   
+  1   4   6   4   1   
+1   5   10   10   5   1   
+
+
+
+<<<<<QUESTION 3>>>>>
+Enter 2 numbers: 34 23
+a. Checking if the quotient and remainder is a callable value or not.
+False
+False
+<b> All the values are non zero
+<c> Filtered out numbers that are greater than 4 are : [5, 15, 16, 6, 16, 7, 17]
+<d> Set: {5, 6, 7, 15, 16, 17}
+<e> Immutable set: frozenset({16, 17, 5, 6, 7, 15})
+<f> Hash value of the max value from the above set: 17
+
+
+
+<<<<<QUESTION 4>>>>>
+Object created
+The name of the student is Yajat  and SID is 21103041
+Object destroyed
+
+
+
+<<<<<QUESTION 5>>>>>
+(a) The updated salary of Yajat is 70000
+(b) Record of Varun deleted
+
+
+<<<<<QUESTION 6>>>>>
+Enter the word: 
+Vivek
+Enter a new meaningful word using the exact same letters to test your friendship: 
+Ashu
+The letters aren't exact, friendship is fake
+
+
+
